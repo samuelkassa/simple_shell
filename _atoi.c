b28bd25aee6e18a0428b4d30c5d5c9d28da505fd
samuelkassa <samuelkassahun22@gmail.com>
2023-02-15 10:06:31 -0800

    Update

---
+++ _atoi.c
@@ -0,0 +1,81 @@
+#include "shell.h"
+
+/**
+ * interactive - returns true if shell is interactive mode
+ * @info: struct address
+ *
+ * Return: 1 if interactive mode, 0 otherwise
+ */
+
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim: It checks if the character is a delimeter
+ * @c: The character to be checked
+ * @delim: The delimeter string
+ *
+ * Return: 1 if true, 0 if false
+ */
+
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+	{
+		if (*delim++ == c)
+			return (1);
+	}
+	return (0);
+}
+
+/**
+ * _isalpha - This function checks if the given argument is
+ * alphabetic character
+ * @c: The argument to be checked
+ *
+ * Return: 1 if c is alphabetic, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * _atoi - This function converts a string in to an integer
+ * @s: The string to be converted
+ *
+ * Return: 0 if no numbers in a string,
+ * otherwise it will return converted number
+ */
+
+int _atoi(char *s)
+{
+	int index, sign = 1, flag = 0, value;
+	unsigned int result = 0;
+
+	for (index = 0; s[index] != '\0' && flag != 2; index++)
+	{
+		if (s[index] == '-')
+			sign *= -1;
+		if (s[index] >= '0' && s[index] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[index] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		value = -result;
+	else
+		value = result;
+	return (value);
+}
---
+++ shell.h
@@ -0,0 +1,256 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+/* Defining Macro for Read/Write buffers */
+
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+/* Defining Macro for Command Chaining */
+#define CMD_NORM 0
+#define CMD_OR 1
+#define CMD_AND 2
+#define CMD_CHAIN 3
+
+/* Defining Macro for Convert_numbers() */
+
+#define CONVERT_LOWERCASE 1
+#define CONVERT_UNSIGNED 2
+
+/* 1 if using system getline() */
+
+#define USE_GETLINE 0
+#define USE_STRTOK 0
+
+#define HIST_FILE ".simple_shell_history"
+#define HIST_MAX 4096
+
+extern char **environ;
+
+/**
+ * struct liststr - singly linked list
+ * @num: The number field
+ * @str: A string
+ * @next: Pointer to the next node or it's link part of the linked list
+ */
+typedef struct liststr
+{
+	int num;
+	char *str;
+	struct liststr *next;
+} list_t;
+
+/**
+ * struct passinfo - Contains pseudo-argumengts for a function,
+ * allowing uniform prototype for function pointer struct
+ * @arg: A string generated from getline containing arguments
+ * @argv: An array of strings generated from arg
+ * @path: A string path for the current command
+ * @argc: The argument count
+ * @line_count: The error count
+ * @err_num: The error code for exit()s
+ * @linecount_flag: if on count this line of input
+ * @fname: The program filename
+ * @env: linked list local copy of environ
+ * @environ: Custrom modified copy of environ from Linked list env
+ * @history: A node containing history
+ * @alias: A node containing alias
+ * @env_changed: On if environ was changed
+ * @status: The return status of the last executed command
+ * @cmd_buf: Address of pointer to cmd_buf, on if chaining
+ * @cmd_buf_type: CMD_type ||, &&, ;
+ * @readfd: The fd or file discriptor from which to read line input
+ * @histcount: The history line number count
+ */
+typedef struct passinfo
+{
+	char *arg;
+	char **argv;
+	char *path;
+	int argc;
+	unsigned int line_count;
+	int err_num;
+	int linecount_flag;
+	char *fname;
+	list_t *env;
+	list_t *history;
+	list_t *alias;
+	char **environ;
+	int env_changed;
+	int status;
+
+	char **cmd_buf; /* pointer to cmd; chain buffer, for memory management */
+	int cmd_buf_type; /* CMD_type ||, &&, ; */
+	int readfd;
+	int histcount;
+} info_t;
+
+#define INFO_INIT \
+{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
+		0, 0, 0}
+
+/**
+ * sturct builtin - Contains a buitlin string and relate function
+ * @type: The builtin command flag
+ * @func: The function
+ */
+typedef struct builtin
+{
+	char *type;
+	int (*func)(info_t *);
+} builtin_table;
+
+/* Defining PROTOTYPE of functions in a given file */
+
+/* shell_loop.c */
+
+int hsh(info_t *, char **);
+int find_builtin(info_t *);
+void find_cmd(info_t *);
+void fork_cmd(info_t *);
+
+/* parser.c */
+
+int is_cmd(info_t *, char *);
+char *dup_chars(char *, int, int);
+char *find_path(info_t *, char *, char *);
+
+/* loophsh.c */
+
+int loophs(char **);
+
+/* erros.c */
+
+void _eputs(char *);
+int _eputchar(char);
+int _putfd(char c, int fd);
+int _putsfd(char *str, int fd);
+
+/* string.c */
+
+int _strlen(char *);
+int _strcmp(char *);
+char *starts_with(const char *, const char *);
+char *_strcat(char *, char *);
+
+/* string1.c */
+
+char *_strcpy(char *, char *);
+char *_strdup(const char *);
+void _puts(char *);
+int _putchar(char);
+
+/* exits.c */
+
+char *_strncpy(char *, char *, int);
+char *_strncat(char *, char *, int);
+char *_strchr(char *, char);
+
+/* tokenizer.c */
+
+char **strlow(char *, char *);
+char **strlow2(char *, char);
+
+/* realloc.c */
+
+char *_memset(char *, char, unsigned int);
+void ffree(char **);
+void *_realloc(void *, unsigned int, unsigned int);
+
+/* memory.c */
+
+int bfree(void **);
+
+/* _atoi.c */
+
+int interactive(info_t *);
+int is_delim(char, char *);
+int _isalpha(int);
+int _atoi(char *);
+
+/* errors1.c */
+
+int _erratoi(char *);
+void print_error(info_t *, char *);
+int print_d(int, int);
+char *convert_number(long int, int, int);
+void remove_comments(char *);
+
+/* builtin.c */
+
+int _myexit(info_t *);
+int _mycd(info_t *);
+int _myhelp(info_t *);
+
+/* builtin1.c */
+int _myhistory(info_t *);
+int _myalias(info_t *);
+
+/* getline.c */
+ssize_t get_input(info_t *);
+int _getline(info_t *, char **, size_t *);
+void sigintHandler(int);
+
+/* geinfo.c */
+void clear_info(info_t *);
+void set_info(info_t *, char **);
+void free_info(info_t *, const char *);
+
+/* environ.c */
+char *_getenv(info_t *, const char *);
+int _myenv(info_t *);
+int _mysetenv(info_t *);
+int _myunsetenv(info_t *);
+int populate_env_list(info_t *);
+
+/* getenv.c */
+char **get_environ(info_t *);
+int _unsetenv(info_t *, char *);
+int _setenv(info_t *, char *, char *);
+
+/* history.c */
+
+char *get_history_file(info_t *info);
+int write_history(info_t *info);
+int read_history(info_t *info);
+int build_history_list(info_t *info, char *buf, int linecount);
+int renumber_history(info_t *info);
+
+/* lists */
+
+list_t *add_node(list_t **, const char *, int);
+list_t *add_node_end(list_t **, const char *, int);
+size_t print_list_str(const list_t *);
+int delete_node_at_index(list_t **, unsigned int);
+void free_list(list_t **);
+
+/* lists1.c */
+
+size_t list_len(const list_t *);
+char **list_to_strings(list_t *);
+size_t print_list(const list_t *);
+list_t *node_starts_with(list_t *,  char, char);
+ssize_t get_node_index(list_t *, list_t *);
+
+/* vars.c */
+
+int is_chain(info_t *, char *, size_t *);
+void check_chain(info_t *, char *, size_t *, size_t, size_t);
+int replace_alias(info_t *);
+int replace_vars(info_t *);
+int replace_string(char **, char *);
+
+#endif
+

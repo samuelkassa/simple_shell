59d644da6acd7b6c4bcfcb0b9c21dd2020f33f5e
AdisalemBayu2112 <adisalembayu@gmail.com>
2023-02-22 01:48:36 -0800

    Updating parser.c file

--- parser.c
+++ parser.c
@@ -68,7 +68,7 @@ char *find_path(info_t *info, char *pathstr, char *cmd)
 	{
 		if (!pathstr[i] || pathstr[i] == ':')
 		{
-			path = dup_chars(pathstr, curr_pos,i);
+			path = dup_chars(pathstr, curr_pos, i);
 			if (!*path)
 				_strcat(path, cmd);
 			else
